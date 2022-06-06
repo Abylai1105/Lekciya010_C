ace38ee61e532984673c82b33ff507a2aa486ffa
Abylai <aba.omar@mail.ru>
2022-06-06 12:44:24 +0600

    Task070

--- Task070/Program.cs
+++ Task070/Program.cs
@@ -3,3 +3,25 @@
 // 3 и 4, N = 5 -> 3 4 7 11 18
 // 6 и 10, N = 4 -> 6 10 16 26 
 
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество чисел s");
+int s = Convert.ToInt32(Console.ReadLine());
+void FibonacciModified(int n, int m,int s)
+{
+    if (s==2)
+        Console.Write($"{n} {m}");
+    else
+    {
+        Console.Write(n + " " );
+        FibonacciModified(m, m+n, s-1);
+    }
+}
+
+FibonacciModified(n,m,s);
+  
+
